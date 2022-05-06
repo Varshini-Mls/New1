230c6e7de69ee17c642871feccea9fb990abc777
varshini.madduri26@outlook.com <DELL@DESKTOP-6MBMD32>
2022-05-06 14:23:43 +0530

    first one

---
+++ newproj/src/newproj/fundamentals1.java
@@ -0,0 +1,17 @@
+package newproj;
+
+import java.util.Scanner;
+
+public class fundamentals1 {
+
+	public static void main(String[] args) {
+		int a = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number: ");
+		a = sc.nextInt();
+		
+		System.out.println("it is integer");
+
+	}
+
+}
